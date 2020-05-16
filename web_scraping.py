6f4a81b09a1345ae57a627268cca2918dba9fee1
joenguyen0109 <joenguye0109@gmail.com>
2020-05-17 02:01:31 +1000

    get all the links and put them into a file

--- web_scraping.py
+++ web_scraping.py
@@ -2,62 +2,42 @@ import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
+# chcp 65001
+# set PYTHONIOENCODING=utf-8
+with open('giao-duc.txt','w') as f:
+	i = 0
+	
+	urls = ['https://vnexpress.net/giao-duc','https://vnexpress.net/giao-duc-p2','https://vnexpress.net/giao-duc-p3']
+	links = []
+	for url in urls:
+		if i > 50:
+				break
+		uClient = uReq(url)
 
-# url = 'https://vnexpress.net/phap-luat'
-# uClient = uReq(url)
+		page_html = uClient.read()
 
-# page_html = uClient.read()
+		uClient.close()
 
-# uClient.close()
+		page_soup = soup(page_html,"html.parser")
+		if url == 'https://vnexpress.net/giao-duc':
+			containers = page_soup.findAll("h4",{"class":"title-news"})
+		else:
+			containers = page_soup.findAll("h3",{"class":"title-news"})
+		for container in containers:
+			f.write(container.a['href'] + '\n')
+			links.append(container.a['href'])
+			i += 1
+			if i > 50:
+				break
+			
+	
 
-# page_soup = soup(page_html,"html.parser")
-
-# container = page_soup.findAll("h4",{"class":"title-news"})
-
-# print(container[0].get_text())
-
-# import bs4
-# from urllib.request import urlopen as uReq
-# from bs4 import BeautifulSoup as soup
-
-
-urls = ['https://vnexpress.net/suc-khoe','https://vnexpress.net/giai-tri']
-topic = {}
-for url in urls:
-	title_holder  = []
-	uClient = uReq(url)
-
-	page_html = uClient.read()
-
-	uClient.close()
-
-	page_soup = soup(page_html,"html.parser")
-
-	titles = page_soup.findAll("h3",{"class":"title-news"})
-
-	for title in titles:
-		title_holder.append(title.get_text().strip())
-
-	print(len(title_holder))
-	topic[url] = title_holder
+	
 
 
 
-urls = ['https://vnexpress.net/phap-luat']
-for url in urls:
-	title_holder  = []
-	uClient = uReq(url)
 
-	page_html = uClient.read()
 
-	uClient.close()
 
-	page_soup = soup(page_html,"html.parser")
 
-	titles = page_soup.findAll("h4",{"class":"title-news"})
 
-	for title in titles:
-		title_holder.append(title.get_text().strip())
-	topic[url] = title_holder
-	print(len(title_holder))
-print(topic)
